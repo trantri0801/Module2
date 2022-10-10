d2b09a244ea4d2044bc3df7c0e5f228d1d453dfa
trantri0801 <tranngoctri8197@gmail.com>
2022-10-10 11:35:29 +0700

    add file

---
+++ IntroductiontoJava/ChangeMoney.java
@@ -0,0 +1,15 @@
+package IntroductiontoJava;
+
+import java.util.Scanner;
+
+public class ChangeMoney{
+    public static void main(String[] args){
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Nhap so tien bang USD: ");
+        usd = sc.nextDouble();
+        double change = usd * vnd;
+        System.out.print("Gia tri VND bang " + change);
+    }
+}
