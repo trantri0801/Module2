6af402cd89fc610cbfd64c88994b91fa5b620ea9
trantri0801 <tranngoctri8197@gmail.com>
2022-11-07 19:09:30 +0700

    add file

--- DSA/LinkedList/MyLinkedList.java
+++ DSA/LinkedList/MyLinkedList.java
@@ -1,4 +1,72 @@
 package DSA.LinkedList;
 
-public class MyLinkedList {
+public class MyLinkedList<E> {
+    private class Node{
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+        public Object getData() {
+            return this.data;
+        }
+    }
+    //phần tử đầu tiên của danh sách liên kết
+    private Node head;
+    //Số lượng phần tử trong danh sách
+    private int numNodes = 0;
+
+    public MyLinkedList(){
+
+    }
+
+    public void addFirst(E element){
+        //khai báo 1 biến là temp trỏ đến giá trị của head
+        Node temp = head;
+        //Biến head sẽ nhận giá trị là 1 node mới
+        head = new Node(element);
+        //head.next = temp;
+        head.next = temp;
+        numNodes++;
+    }
+
+    public void addLast(E element){
+        //khai báo biến temp trỏ đến head
+        Node temp = head;
+        //Sẽ cho con trỏ chạy đến phần tử cuối cùng của danh sách
+        while (temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = new Node(element);
+        numNodes++;
+    }
+    public void add(int index, E element){
+        //khai báo biến temp trỏ đến head
+        Node temp = head;
+        //Khai báo 1 node là holder
+        Node holder;
+        //Cho con trỏ chạy đến vị trí index -1
+        for(int i = 0; i < index -1 && temp.next != null; i++){
+            temp = temp.next;
+        }
+        //cho holder tham chiếu đến vị trí index
+        holder = temp.next;
+        //Node tại vị trí index -1 sẽ trỏ tới node mới
+        temp.next = new Node(element);
+        //node mới sẽ trỏ đến holder
+        temp.next.next = holder;
+    }
+    public Object get(int index){
+        //khai báo biến temp trỏ tới head
+        Node temp = head;
+        for (int i=0; i < index;i++){
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+
+    public int size(){
+        return numNodes;
+    }
 }
--- DSA/LinkedList/MyLinkedListTest.java
+++ DSA/LinkedList/MyLinkedListTest.java
@@ -1,4 +1,46 @@
 package DSA.LinkedList;
 
 public class MyLinkedListTest {
+    public static void main(String[] args) {
+        class Student {
+            private int id;
+            private String name;
+
+            public Student(int id, String name) {
+                this.id = id;
+                this.name = name;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public Student() {
+
+            }
+        }
+        MyLinkedList<Student> myLinkedList = new MyLinkedList<Student>();
+        Student student1 = new Student(1, "Tien");
+        Student student2 = new Student(2, "Hai");
+        Student student3 = new Student(3, "Trung");
+        Student student4 = new Student(4, "Thanh");
+        myLinkedList.addFirst(student1);
+
+        for (int i = 0; i < myLinkedList.size(); i++){
+            Student student = (Student) myLinkedList.get(i);
+            System.out.printf(student.getName());
+        }
+    }
 }
